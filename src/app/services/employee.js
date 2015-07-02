a45517c486d098136c10e7a912bb29bedb99ed05
d.karpenkov <d.karpenkov@itransition.com>
2015-07-02 10:51:08 +0300

    Updated employees data

--- src/app/services/employee.js
+++ src/app/services/employee.js
@@ -6,157 +6,379 @@ var moment = require('moment');
 var _employees = [
     {
         id: 1,
-        name: 'David Flanagan',
+        name: 'Atroshkin, Igor',
         projects: [
             {
-                id: 3,
-                title: 'Itransition RFX Processing',
-                date_start: +moment('2015-03-25'),
-                date_end: null
-            },
-            {
-                id: 1,
-                title: 'Selfeducation',
-                date_start: +moment('2014-03-25'),
-                date_end: null
-            },
-            {
-                id: 6,
-                title: 'DSM',
-                date_start: +moment('2014-03-14'),
-                date_end: +moment('2015-06-22')
+                id: 2,
+                title: 'Test Period',
+                date_start: +moment('2015-07-08'),
+                date_end: +moment('2015-08-07')
             }
         ]
     },
     {
         id: 2,
-        name: 'John Doe',
+        name: 'Bozhik, Stepan',
         projects: [
             {
-                id: 3,
-                title: 'Itransition RFX Processing',
-                date_start: +moment('2015-06-01'),
-                date_end: +moment('2015-07-20')
+                id: 10001,
+                title: 'Paxton Access - Net10 UI',
+                date_start: +moment('2014-06-01'),
+                date_end: null
             }
         ]
     },
     {
         id: 3,
-        name: 'John Smith',
+        name: 'Bronishevsky, Vladislav',
         projects: [
             {
-                id: 1,
-                title: 'Selfeducation',
-                date_start: +moment('2015-04-30'),
-                date_end: +moment('2015-12-25')
+                id: 10002,
+                title: 'Accela - BoardTraq',
+                date_start: +moment('2014-05-01'),
+                date_end: +moment('2015-04-30')
             },
             {
-                id: 3,
-                title: 'Itransition RFX Processing',
-                date_start: +moment('2015-02-25'),
-                date_end: +moment('2016-03-25')
+                id: 10003,
+                title: 'Wren Kitchens - Planner',
+                date_start: +moment('2015-05-01'),
+                date_end: null
             }
         ]
     },
     {
         id: 4,
-        name: 'Martin Fuller',
+        name: 'Vasiliskin, Evgeny',
         projects: [
             {
-                id: 4,
-                title: 'DSM',
-                date_start: +moment('2014-03-14'),
-                date_end: +moment('2015-09-25')
-            },
-            {
-                id: 5,
+                id: 10004,
                 title: 'IRIS',
-                date_start: +moment('2014-03-25'),
-                date_end: +moment().add(7, 'd')
+                date_start: +moment('2015-05-12'),
+                date_end: +moment('2015-09-01')
             },
             {
-                id: 2,
-                title: 'Absence',
-                date_start: +moment('2015-05-25'),
-                date_end: +moment('2015-06-20')
+                id: 10005,
+                title: 'Forex',
+                date_start: +moment('2014-09-01'),
+                date_end: +moment('2015-05-11')
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Vasilyev, Aleksey',
+        projects: [
+            {
+                id: 10006,
+                title: 'Themis',
+                date_start: +moment('2014-09-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: 'Volchetsky, Sergey',
+        projects: [
+            {
+                id: 1,
+                title: 'Selfeducation',
+                date_start: +moment('2015-06-01'),
+                date_end: +moment('2015-07-12')
+            },
+			{
+                id: 10007,
+                title: 'ZigZag',
+                date_start: +moment('2015-07-15'),
+                date_end: +moment('2015-08-15')
+            }
+        ]
+    },
+    {
+        id: 7,
+        name: 'Gorbach, Yuri',
+        projects: [
+            {
+                id: 10006,
+                title: 'Themis',
+                date_start: +moment('2015-04-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 8,
+        name: 'Grischenko, Anton',
+        projects: [
+            {
+                id: 10008,
+                title: 'SDVentures',
+                date_start: +moment('2014-03-14'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 9,
+        name: 'Gulidov, Vladimir',
+        projects: [
+            {
+                id: 10003,
+                title: 'Wren Kitchens - Planner',
+                date_start: +moment('2015-06-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 9,
+        name: 'Gulidov, Vladimir',
+        projects: [
+            {
+                id: 10003,
+                title: 'Wren Kitchens - Planner',
+                date_start: +moment('2015-06-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 10,
+        name: 'Derevyago, Evgeny',
+        projects: [
+            {
+                id: 1,
+                title: 'Selfeducation',
+                date_start: +moment('2015-06-01'),
+                date_end: null
             }
         ]
     },
     {
         id: 11,
-        name: 'David Flanagan',
+        name: 'Zinovyev, Evgeny',
         projects: [
             {
-                id: 3,
-                title: 'Itransition RFX Processing',
-                date_start: +moment('2015-03-25'),
-                date_end: +moment('2015-06-29')
+                id: 10,
+                title: 'Annual Leave',
+                date_start: +moment('2015-06-29'),
+                date_end: +moment('2015-07-16')
             },
+			{
+                id: 10009,
+                title: 'Kaspersky',
+                date_start: +moment('2014-09-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 12,
+        name: 'Zmitrovich, Evgeny',
+        projects: [
             {
-                id: 6,
-                title: 'WD Hiring Activities',
-                date_start: +moment('2015-04-14'),
-                date_end: +moment().add(7, 'd')
+                id: 10006,
+                title: 'Themis',
+                date_start: +moment('2014-09-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 13,
+        name: 'Karaychenstev, Vladimir',
+        projects: [
+            {
+                id: 10006,
+                title: 'Themis',
+                date_start: +moment('2015-05-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 14,
+        name: 'Korol, Sergey',
+        projects: [
+            {
+                id: 10010,
+                title: 'Becton Dickinson',
+                date_start: +moment('2014-05-01'),
+                date_end: +moment('2014-06-30')
             },
+			{
+                id: 10010,
+                title: 'QuantumVerse',
+                date_start: +moment('2014-07-01'),
+                date_end: +moment('2014-08-31')
+            },
+			{
+                id: 10,
+                title: 'Annual Leave',
+                date_start: +moment('2014-07-13'),
+                date_end: +moment('2014-07-28')
+            }
+        ]
+    },
+    {
+        id: 15,
+        name: 'Korzun, Yuri',
+        projects: [
             {
-                id: 5,
-                title: 'IRIS',
+                id: 10001,
+                title: 'Paxton Access - Net10 UI',
+                date_start: +moment('2014-09-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 16,
+        name: 'Krischik, Ignat',
+        projects: [
+            {
+                id: 1,
+                title: 'Selfeducation',
+                date_start: +moment('2014-07-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 17,
+        name: 'Labushkina, Anna',
+        projects: [
+            {
+                id: 10005,
+                title: 'Forex',
+                date_start: +moment('2014-09-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 18,
+        name: 'Legovich, Andrey',
+        projects: [
+            {
+                id: 10008,
+                title: 'SDVentures',
                 date_start: +moment('2014-03-14'),
-                date_end: +moment('2015-06-22')
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 19,
+        name: 'Maslovsky, Vadim',
+        projects: [
+            {
+                id: 1,
+                title: 'Selfeducation',
+                date_start: +moment('2015-06-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 20,
+        name: 'Mironov, Georgy',
+        projects: [
+            {
+                id: 100011,
+                title: 'Productive Edge',
+                date_start: +moment('2015-05-01'),
+                date_end: null
             }
         ]
     },
     {
         id: 21,
-        name: 'John Doe',
+        name: 'Nekazakova, Natalya',
         projects: [
             {
-                id: 3,
-                title: 'Itransition RFX Processing',
-                date_start: +moment('2015-06-01'),
-                date_end: +moment('2015-07-15')
+                id: 100012,
+                title: 'Pull Media',
+                date_start: +moment('2015-05-01'),
+                date_end: +moment('2015-07-31')
             }
         ]
     },
     {
-        id: 31,
-        name: 'John Smith',
+        id: 22,
+        name: 'Nikolaychik, Mihail',
         projects: [
             {
-                id: 1,
-                title: 'Selfeducation',
-                date_start: +moment('2015-04-30'),
-                date_end: +moment('2015-12-25')
-            },
-            {
-                id: 3,
-                title: 'Itransition RFX Processing',
-                date_start: +moment('2015-02-25'),
-                date_end: +moment('2016-03-25')
+                id: 100013,
+                title: 'Edyt',
+                date_start: +moment('2014-10-01'),
+                date_end: null
             }
         ]
     },
     {
-        id: 41,
-        name: 'Martin Fuller',
+        id: 23,
+        name: 'Novogrodsky, Denis',
         projects: [
             {
-                id: 3,
-                title: 'Itransition RFX Processing',
+                id: 10006,
+                title: 'Themis',
+                date_start: +moment('2014-10-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 24,
+        name: 'Petrov, Andrey',
+        projects: [
+            {
+                id: 100014,
+                title: 'Accela',
+                date_start: +moment('2015-05-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 25,
+        name: 'Poddubsky, Mihail',
+        projects: [
+            {
+                id: 100001,
+                title: 'Paxton Access - Net10 UI',
+                date_start: +moment('2015-01-01'),
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 26,
+        name: 'Polyakova, Kira',
+        projects: [
+            {
+                id: 10008,
+                title: 'SDVentures',
                 date_start: +moment('2014-03-14'),
-                date_end: +moment('2015-06-25')
-            },
+                date_end: null
+            }
+        ]
+    },
+    {
+        id: 27,
+        name: 'Radyuk, Tatiana',
+        projects: [
             {
+                id: 100015,
+                title: 'OPUS',
+                date_start: +moment('2015-06-01'),
+                date_end: +moment('2015-06-30')
+            },
+			{
                 id: 1,
                 title: 'Selfeducation',
-                date_start: +moment('2014-03-25'),
-                date_end: +moment('2015-07-29')
-            },
-            {
-                id: 2,
-                title: 'Absence',
-                date_start: +moment('2015-03-25'),
-                date_end: +moment('2015-06-20')
+                date_start: +moment('2015-07-01'),
+                date_end: null
             }
         ]
     }
