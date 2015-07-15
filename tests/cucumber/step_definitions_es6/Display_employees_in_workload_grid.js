8a30b38aa2e75efba18970a10828a5319c6f0525
e.yatsuk@itransition.com <e.yatsuk@itransition.com>
2015-07-15 16:02:55 +0300

    RPS-11 Refactored test steps

--- tests/cucumber/step_definitions_es6/Display_employees_in_workload_grid.js
+++ tests/cucumber/step_definitions_es6/Display_employees_in_workload_grid.js
@@ -4,64 +4,50 @@ var Q = require('q');
 var myStepDefinitionsWrapper = function () {
     this.World = require('../support/world.js').World;
 
-    var wrapTest = function (fn) {
-        Q.async(fn)();
+    var wrapTest = function (context, callback, fn) {
+        var promise = Q.async(function(){
+            return fn.call(context);
+        });
+
+        promise().catch(function (err) {
+            callback.fail(err);
+        });
+
     };
 
     this.Given(/^I'm logged in as 'Admin'$/, function (callback) {
-        var self = this;
 
-        wrapTest(function* () {
-
-            try {
-                yield self.browser.get('http://localhost:3000/#/login');
-                var emailInput = yield self.browser.elementById('auth-email-input');
-                var passwordInput = yield self.browser.elementById('auth-password-input');
-                emailInput.value = 'example@email.com';
-                passwordInput.value = 'password';
-                var loginButton = self.browser.elementByClassName('login-btn');
-                yield loginButton.click();
-                callback();
-            } catch (error) {
-                callback.fail(error);
-            }
-
+        wrapTest(this, callback, function* () {
+            yield this.browser.get('http://localhost:3000/#/login');
+            var emailInput = yield this.browser.elementById('auth-email-input');
+            var passwordInput = yield this.browser.elementById('auth-password-input');
+            emailInput.value = 'example@email.com';
+            passwordInput.value = 'password';
+            var loginButton = this.browser.elementByClassName('login-btn');
+            yield loginButton.click();
+            callback();
         });
 
     });
 
     this.When('I navigate to "$module" module', function (module, callback) {
-        var self = this;
 
-        wrapTest(function* () {
-
-            try {
-                yield self.browser.get('http://localhost:3000/#/' + module);
-                callback();
-            } catch (error) {
-                callback.fail('Navigate to ' + module + ' fail ' + err);
-            }
-
+        wrapTest(this, callback, function* () {
+            yield this.browser.get('http://localhost:3000/#/' + module);
+            callback();
         });
 
     });
 
     this.Then(/^I see table of employees$/, function (callback) {
-        var self = this;
 
-        wrapTest(function* () {
+        wrapTest(this, callback, function* () {
+            var workloadGrid = yield this.browser.waitForElementByClassName('workload-grid');
 
-            try {
-                var workloadGrid = yield self.browser.waitForElementByClassName('workload-grid');
-
-                if (workloadGrid) {
-                    callback();
-                } else {
-                    callback.fail('Find workload table fail ' + err);
-                }
-
-            } catch (error) {
-                callback.fail('Find workload table fail ' + error);
+            if (workloadGrid) {
+                callback();
+            } else {
+                callback.fail('Find workload table fail ' + err);
             }
 
         });
@@ -69,26 +55,21 @@ var myStepDefinitionsWrapper = function () {
     });
 
     this.Then(/^table contains columns$/, function (callback) {
-        var self = this;
 
-        wrapTest(function* () {
-            try {
-                var workloadGrid = yield self.browser.elementByClassName('workload-grid');
+        wrapTest(this, callback, function* () {
+            var workloadGrid = yield this.browser.elementByClassName('workload-grid');
 
-                if (workloadGrid) {
-                    var rows = yield workloadGrid.elementsByCssSelector('tr');
+            if (workloadGrid) {
+                var rows = yield workloadGrid.elementsByCssSelector('tr');
 
-                    if (rows.slice().length) {
-                        callback();
-                    } else {
-                        callback.fail();
-                    }
-
+                if (rows.slice().length) {
+                    callback();
+                } else {
+                    callback.fail();
                 }
 
-            } catch (error) {
-                callback.fail('Find workload table rows fail ' + err);
             }
+
         });
 
     });
