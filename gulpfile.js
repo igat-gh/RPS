e9be6c00bb0c58b0f93e3cd5908ac41fa3ad4336
e.yatsuk@itransition.com <e.yatsuk@itransition.com>
2015-07-31 16:33:26 +0300

    RPS-12 Gulp file transformed to meet the requirements

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,12 @@ var notify = require('gulp-notify');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var gutil = require('gulp-util');
-var shell = require('gulp-shell');
 var glob = require('glob');
 var livereload = require('gulp-livereload');
 var jshint = require('gulp-jshint');
 var react = require('gulp-react');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
-var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
 var babel = require('gulp-babel');
 var jsdoc = require('gulp-jsdoc');
 var exec = require('gulp-exec');
@@ -27,42 +25,49 @@ var del = require('del');
 // External dependencies you do not want to rebundle while developing,
 // but include in your application deployment
 var dependencies = [
+    'flux',
     'react',
-    'react/addons'
+    'react/addons',
+    'object-assign',
+    'es6-promise',
+    'moment'
 ];
 
-var browserifyTask = function (options) {
+var environment = {
+    development: 'development',
+    test: 'test',
+    release: 'release'
+};
 
+var browserifyTask = function (options) {
     // Our app bundler
     var appBundler = browserify({
         entries: [options.src], // Only need initial file, browserify finds the rest
         transform: [reactify], // We want to convert JSX to normal javascript
-        debug: options.development, // Gives us sourcemapping
-        cache: {}, packageCache: {}, fullPaths: options.development // Requirement of watchify
-    });
-
-    // We set our dependencies as externals on our app bundler when developing
-    (options.development ? dependencies : []).forEach(function (dep) {
-        appBundler.external(dep);
+        cache: {}, packageCache: {}, fullPaths: options.environment !== environment.release // Requirement of watchify
     });
 
     // The rebundle process
     var rebundle = function () {
         var start = Date.now();
         console.log('Building APP bundle');
-        appBundler.bundle()
+        appBundler
+            .external(dependencies)
+            .bundle()
             .on('error', gutil.log)
             .pipe(source('main.js'))
-            .pipe(gulpif(!options.development, streamify(uglify())))
+            .pipe(gulpif(options.environment !== environment.release, streamify(sourcemaps.init())))
+            .pipe(gulpif(options.environment !== environment.development, streamify(uglify())))
+            .pipe(streamify(sourcemaps.write()))
             .pipe(gulp.dest(options.dest))
-            .pipe(gulpif(options.development, livereload()))
+            .pipe(gulpif(options.environment === environment.development, livereload()))
             .pipe(notify(function () {
                 console.log('APP bundle built in ' + (Date.now() - start) + 'ms');
             }));
     };
 
     // Fire up Watchify when developing
-    if (options.development) {
+    if (options.environment === environment.development) {
         appBundler = watchify(appBundler);
         appBundler.on('update', rebundle);
     }
@@ -70,32 +75,29 @@ var browserifyTask = function (options) {
     rebundle();
 
     // We create a separate bundle for our dependencies as they
-    // should not rebundle on file changes. This only happens when
-    // we develop. When deploying the dependencies will be included
-    // in the application bundle
-    if (options.development) {
+    // should not rebundle on file changes.
+    var vendorsBundler = browserify({
+        require: dependencies
+    });
 
-        var vendorsBundler = browserify({
-            debug: true,
-            require: dependencies
-        });
-
-        // Run the vendor bundle
-        var start = new Date();
-        console.log('Building VENDORS bundle');
-        vendorsBundler.bundle()
-            .on('error', gutil.log)
-            .pipe(source('vendors.js'))
-            .pipe(gulp.dest(options.dest))
-            .pipe(notify(function () {
-                console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
-            }));
-    }
+    // Run the vendor bundle
+    var start = new Date();
+    console.log('Building VENDORS bundle');
+    vendorsBundler.bundle()
+        .on('error', gutil.log)
+        .pipe(source('vendors.js'))
+        .pipe(gulpif(options.environment !== environment.release, streamify(sourcemaps.init())))
+        .pipe(gulpif(options.environment !== environment.development, streamify(uglify())))
+        .pipe(streamify(sourcemaps.write()))
+        .pipe(gulp.dest(options.dest))
+        .pipe(notify(function () {
+            console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
+        }));
 
 };
 
 var cssTask = function (options) {
-    if (options.development) {
+    if (options.environment === environment.development) {
         var run = function () {
             var start = new Date();
             console.log('Building CSS bundle');
@@ -113,9 +115,11 @@ var cssTask = function (options) {
         gulp.watch(options.src, run);
     } else {
         gulp.src(options.src)
+            .pipe(gulpif(options.environment === environment.test, sourcemaps.init()))
             .pipe(less())
             .pipe(concat(options.name + '.css'))
             .pipe(cssmin())
+            .pipe(sourcemaps.write())
             .pipe(gulp.dest(options.dest));
     }
 };
@@ -124,7 +128,7 @@ var htmlTask = function (options) {
     var start = new Date();
     console.log('Building HTML bundle');
     gulp.src(options.src)
-        .pipe(preprocess({context: {DEVELOPMENT: options.development}})) //To set environment variables in-line
+        .pipe(preprocess({context: {DEVELOPMENT: options.environment === environment.development}})) //To set environment variables in-line
         .pipe(gulp.dest(options.dest))
         .pipe(notify(function () {
             console.log('HTML bundle built in ' + (Date.now() - start) + 'ms');
@@ -153,74 +157,113 @@ gulp.task('default', ['jshint', 'jsdoc'], function () {
         },
         function () {
             browserifyTask({
-                development: true,
+                environment: environment.development,
                 src: './src/app/main.js',
                 dest: './build/dev/scripts'
             });
 
             cssTask({
-                development: true,
+                environment: environment.development,
                 src: ['./src/styles/main.less'],
                 name: 'styles',
                 dest: './build/dev/styles'
             });
 
             cssTask({
-                development: true,
+                environment: environment.development,
                 src: ['./node_modules/bootstrap/less/bootstrap.less'],
                 name: 'libs',
                 dest: './build/dev/styles'
             });
 
             htmlTask({
-                development: true,
+                environment: environment.development,
                 src: './src/app/index.html',
                 dest: './build/dev'
             });
 
             assetsTask({
-                development: true,
+                environment: environment.development,
                 src: './src/assets/**/*',
                 dest: './build/dev/assets'
             });
         });
 });
 
+//Test build
+gulp.task('test', ['jshint'], function () {
+    cleanUpTask({
+            path: ['./build/test/']
+        },
+        function () {
+            browserifyTask({
+                environment: environment.test,
+                src: './src/app/main.js',
+                dest: './build/test/scripts'
+            });
+
+            cssTask({
+                environment: environment.test,
+                src: ['./src/styles/main.less'],
+                name: 'styles',
+                dest: './build/test/styles'
+            });
+
+            cssTask({
+                environment: environment.test,
+                src: ['./node_modules/bootstrap/less/bootstrap.less'],
+                name: 'libs',
+                dest: './build/test/styles'
+            });
+
+            htmlTask({
+                environment: environment.test,
+                src: './src/app/index.html',
+                dest: './build/test'
+            });
+
+            assetsTask({
+                environment: environment.test,
+                src: './src/assets/**/*',
+                dest: './build/test/assets'
+            });
+        });
+});
+
 // Production build
-gulp.task('deploy', ['jshint'], function () {
+gulp.task('release', ['jshint'], function () {
     cleanUpTask({
             path: ['./build/prod/']
         },
         function () {
             browserifyTask({
-                development: false,
+                environment: environment.release,
                 src: './src/app/main.js',
                 dest: './build/prod/scripts'
             });
 
             cssTask({
-                development: false,
+                environment: environment.release,
                 src: ['./src/styles/main.less'],
                 name: 'styles',
                 dest: './build/prod/styles'
             });
 
             cssTask({
-                development: false,
+                environment: environment.release,
                 src: ['./node_modules/bootstrap/less/bootstrap.less'],
                 name: 'libs',
                 dest: './build/prod/styles'
             });
 
-
             htmlTask({
-                development: false,
+                environment: environment.release,
                 src: './src/app/index.html',
                 dest: './build/prod'
             });
 
             assetsTask({
-                development: false,
+                environment: environment.release,
                 src: './src/assets/**/*',
                 dest: './build/prod/assets'
             });
@@ -233,29 +276,6 @@ gulp.task('jshint', function () {
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'));
-});
-
-// Build tests
-// To see test results open /project-root/build/test/testrunner-phantomjs.html in your browser
-gulp.task('test', function () {
-    var testFiles = glob.sync('./src/specs/**/*-spec.js');
-    var testBundler = browserify({
-        entries: testFiles,
-        debug: true,
-        transform: [reactify],
-        cache: {}, packageCache: {}, fullPaths: true
-    });
-
-    var start = Date.now();
-    console.log('Building TEST bundle');
-    testBundler.bundle()
-        .on('error', gutil.log)
-        .pipe(source('specs.js'))
-        .pipe(gulp.dest('./build/test'))
-        .pipe(notify(function () {
-            console.log('TEST bundle built in ' + (Date.now() - start) + 'ms');
-        }));
-
 });
 
 var transformEs6TestStepsTask = function () {
